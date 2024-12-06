35b0c2e554bc2c1f45be4039a925b9598053ab71
Laiana angel <laianaangel57@gmail.com>
2024-12-06 14:27:41 -0300

    Add files via upload

---
+++ atividade5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero;
+
+        do
+        {
+            Console.Write("Digite um número: ");
+            if (int.TryParse(Console.ReadLine(), out numero))
+            {
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("Parabéns! Você digitou um número par.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("O número é ímpar. Tente novamente.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, insira um número válido.");
+            }
+        } while (true);
+    }
+}
+
